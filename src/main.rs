385e80f166842c77fee886e746eb2fedd4be0301
rektstars <rektstarsceosu@disroot.org>
2026-04-16 00:22:10 +0300

    added scroll jitter

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use axum::{
 use tower_http::services::{ServeDir};
 use std::fs;
 use regex::Regex;
+use std::sync::Mutex;
+use rand::RngExt;
+
+const DELAY_MAX: f32 = 0.1;
 
 #[tokio::main]
 async fn main() {
@@ -30,6 +34,7 @@ async fn handler_404() -> (StatusCode, Html<String>) {
 }
 
 async fn handler() -> Html<String> {
+    jitter(-1.0);
     let mut html = fs::read_to_string("templates/example.html").unwrap();
     let re = Regex::new(r#"<div class="line">\[\s*([0-9.]+)\s*\]"#).unwrap();
     html = re.replace_all(&html, // FIND ALL THE MATCH INJECT DELAY (and itself because replace_all destroys the match?)
@@ -37,9 +42,25 @@ async fn handler() -> Html<String> {
             let delay_str = &caps[1];
             let delay: f32 = delay_str.parse().unwrap_or(0.0);
             return format!(r#"<div class="line" style="--delay: {}s;"><span class="timestamp">[{:>12.6}]</span>"#,
-            delay, delay);
+            jitter(delay), delay);
         }
     ).to_string();
-
     return Html(html);
+}
+
+fn jitter(delay: f32) -> f32 {
+    static LAST: Mutex<f32> = Mutex::new(0.0); // wont run again hopefully
+    let mut last = LAST.lock().unwrap();
+    // reset the jitter
+    if delay == -1.0 {
+        *last = 0.0;
+        return -1.0;
+    }
+    let mut rng = rand::rng();
+    if *last > delay {
+        *last += rng.random_range(0.0..DELAY_MAX);
+    } else {
+        *last = delay + rng.random_range(0.0..DELAY_MAX);
+    }
+    return *last;
 }
